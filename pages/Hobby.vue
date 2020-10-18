f8f9773affd50e645fd116d623611834137466e0
Owen <odaley45@cs.washington.edu>
2020-10-17 23:01:49 -0700

    fix

--- pages/Hobby.vue
+++ pages/Hobby.vue
@@ -1,32 +1,33 @@
 <template>
-
+  <div>
+  </div>
 </template>
 <script>
-    export default() {
-        name: 'Hobby',
-        props: {
-            HobbyName: {
-                type: String,
-                required: false,
-                default: 'Hiking'
-            },
-            Number-users: {
-                type: Integer,
-                required: false,
-                default: 0
-            },
-            Number-groups: {
-                type: Integer,
-                required: false,
-                default: 0
-            },
-            img-url: {
-                type: String,
-                required: false,
-                default: '~/static/images/logo.png'
-            }
-        }
+export default {
+  name: 'Hobby',
+  props: {
+    HobbyName: {
+      type: String,
+      required: false,
+      default: 'Hiking'
+    },
+    Numusers: {
+      type: Number,
+      required: false,
+      default: 0
+    },
+    Numgroups: {
+      type: Number,
+      required: false,
+      default: 0
+    },
+    imgurl: {
+      type: String,
+      required: false,
+      default: '~/static/images/logo.png'
     }
+  }
+}
 </script>
 <style>
 </style>
