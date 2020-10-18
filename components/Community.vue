513ebec7cd2360eb8090c80b6ad5c3baf9adce9f
sabethunder8 <sabalmulepati@gmail.com>
2020-10-17 20:45:02 -0700

    Image resize

--- components/Community.vue
+++ components/Community.vue
@@ -2,17 +2,18 @@
   <div>
     <nuxt-link to="/">
       <b-card
-        :title="cardTitle"
-        :img-src="cardImage"
-        img-alt="Image"
-        img-top
+        no-body
         tag="article"
         style="max-width: 20rem;"
         class="mb-2"
       >
-        <b-card-text>
-          Some quick example text to build on the card title and make up the bulk of the card's content.
-        </b-card-text>
+        <b-card-img :src="cardImage" alt="Image" class="image-card"></b-card-img>
+        <b-card-body>
+          <b-card-title>{{ cardTitle }}</b-card-title>
+          <b-card-text>
+            Some quick example text to build on the card title and make up the bulk of the card's content.
+          </b-card-text>
+        </b-card-body>
       </b-card>
     </nuxt-link>
   </div>
@@ -29,5 +30,10 @@ export default {
 </script>
 
 <style scoped>
+.image-card {
+    width: 100%;
+    height: 15vw;
+    object-fit: cover;
+}
 
 </style>
