97acc5d8c79372759e330db9e0f83434a2f278ed
sabethunder8 <sabalmulepati@gmail.com>
2020-10-18 00:49:51 -0700

    community

---
+++ components/Hobbycard.vue
@@ -0,0 +1,49 @@
+<template>
+  <div id="hob">
+    <nuxt-link :to="'/Group/' + cardTitle">
+      <b-card
+        no-body
+        tag="article"
+        style="max-width: 20rem;"
+        class="mb-2"
+      >
+        <b-card-img :src="cardImage" alt="Image" rounded="circle" class="image-card"></b-card-img>
+        <b-card-body>
+          <b-card-title>{{ cardTitle }}</b-card-title>
+          <b-card-text>
+            <p>{{ cardDesc }}</p>
+          </b-card-text>
+        </b-card-body>
+      </b-card>
+    </nuxt-link>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'Hobbycard',
+  props: {
+    cardTitle: { type: String, required: true },
+    cardImage: { type: String, required: true, default: '~/static/images/logo.png' },
+    cardDesc: { type: String, required: false, default: 'Check out this Group!' }
+  }
+}
+</script>
+
+<style scoped>
+.image-card {
+    width: 100%;
+    height: 15vw;
+    object-fit: cover;
+}
+
+.nuxt-link-active {
+  text-decoration: none;
+  color: inherit;
+}
+
+#hob:hover {
+  box-shadow: 0 12px 16px 0 rgba(0,0,0,0.24), 0 16px 24px 0 rgba(0,0,0,0.19);
+}
+
+</style>
