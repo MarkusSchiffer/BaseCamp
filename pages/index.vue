27869ad849eb5da37c9a2ca4b15cc8d2be587a8d
Owen <odaley45@cs.washington.edu>
2020-10-17 19:14:51 -0700

    added button

--- pages/index.vue
+++ pages/index.vue
@@ -2,6 +2,8 @@
   <div class="container">
     <div>
       <img id="logo" src="~/static/images/logo.png" />
+      <p>get started learning a new Hobby with BaseCamp!</p>
+      <button id="start" onClick="document.location='.'">Start Now</button>
       <Community :cardTitle="'Card Title'" :cardImage="'https://picsum.photos/600/300/?image=25'"/>
     </div>
   </div>
@@ -23,7 +25,9 @@ export default {
     width: 200px;
     height: 200px;
     margin: 10px;
-    bottom: 0;
-    right: 0;
+  }
+
+  body {
+    color: red;
   }
 </style>
