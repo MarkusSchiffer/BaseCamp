3f9ac4048685401971208226c3f2eede7023fdf5
Owen <odaley45@cs.washington.edu>
2020-10-17 19:57:27 -0700

    little more css

--- nuxt.config.js
+++ nuxt.config.js
@@ -38,21 +38,21 @@ export default {
     '@nuxtjs/axios',
    
   ],
-  firebase: {
-      config: {
-      apiKey: "AIzaSyBXLnV1fHMqdBmzCs1kuwwhu4bmPW8lFLI",
-      authDomain: "basecamp-70de0.firebaseapp.com",
-      databaseURL: "https://basecamp-70de0.firebaseio.com",
-      projectId: "basecamp-70de0",
-      storageBucket: "basecamp-70de0.appspot.com",
-      messagingSenderId: "890594893991",
-      appId: "1:890594893991:web:0fde4595dcb3f29df23b82",
-      measurementId: "G-7WS9W9ZF0W"
-    },
-    services: {
-      auth: true // Just as example. Can be any other service.
-    }
-  },
+   firebase: {
+       config: {
+       apiKey: "AIzaSyBXLnV1fHMqdBmzCs1kuwwhu4bmPW8lFLI",
+       authDomain: "basecamp-70de0.firebaseapp.com",
+       databaseURL: "https://basecamp-70de0.firebaseio.com",
+       projectId: "basecamp-70de0",
+       storageBucket: "basecamp-70de0.appspot.com",
+       messagingSenderId: "890594893991",
+       appId: "1:890594893991:web:0fde4595dcb3f29df23b82",
+       measurementId: "G-7WS9W9ZF0W"
+     },
+     services: {
+       auth: true // Just as example. Can be any other service.
+     }
+   },
   // Axios module configuration (https://go.nuxtjs.dev/config-axios)
   axios: {},
 
--- pages/index.vue
+++ pages/index.vue
@@ -1,11 +1,15 @@
 <template>
   <div class="container">
-    <div>
-      <img id="logo" src="~/static/images/logo.png" />
-      <p>get started learning a new Hobby with BaseCamp!</p>
-      <button id="start" onClick="document.location='.'">Start Now</button>
-      <Community :cardTitle="'Card Title'" :cardImage="'https://picsum.photos/600/300/?image=25'"/>
+    <div class="row">
+      <div class="col">
+        <p id="intro">get started learning a new Hobby with BaseCamp!</p>
+        <button id="start" onClick="document.location='.'">Start Now</button>
+      </div>
+      <div class="col">
+        <img id="logo" src="~/static/images/logo.png" />
+      </div>
     </div>
+    <Community :cardTitle="'Card Title'" :cardImage="'https://picsum.photos/600/300/?image=25'"/>
   </div>
 </template>
 
@@ -28,6 +32,17 @@ export default {
   }
 
   body {
-    color: red;
+    color: #007189;
+  }
+
+  #intro {
+    font-size: 30px;
+    text-align: center;
+    margin-top: 20px;
+  }
+
+  #start {
+    align: center;
+    font-size: 20px;
   }
 </style>
