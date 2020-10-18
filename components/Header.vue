3741b7f5d64ae8fd0b9faefd0b6dc45db7f6e236
Markus Schiffer <47703802+MarkusSchiffer@users.noreply.github.com>
2020-10-17 23:53:04 -0700

    fixed routing

--- components/Header.vue
+++ components/Header.vue
@@ -14,8 +14,12 @@
         <!-- Right aligned nav items -->
         <b-navbar-nav class="ml-auto">
           <b-nav-form v-if="onHome" class="d-flex ">
-            <b-form-input size="sm" class="mr-sm-2" placeholder="Search"></b-form-input>
-            <b-button id="search" size="sm" class="my-2 my-sm-0" type="submit" @click.stop.prevent="comSearch()">Search</b-button>
+            <b-form-input id="search" size="sm" class="mr-sm-2" placeholder="Search"></b-form-input>
+            <b-button
+            size="sm"
+            class="my-2 my-sm-0"
+            type="submit"
+            @click.stop.prevent="comSearch()">Search</b-button>
           </b-nav-form>
 
           <b-nav-item href="#">
@@ -52,6 +56,11 @@ export default {
       return this.$route.path === '/'
     }
   },
+  data () {
+    return {
+      query: ''
+    }
+  },
   methods: {
     googleSignIn () {
       this.provider = new firebase.auth.GoogleAuthProvider()
@@ -64,7 +73,8 @@ export default {
       })
     },
     comSearch () {
-      this.$router.push('/communities/alpha')
+      const word = document.getElementById('search').value
+      this.$router.push('/communities/' + word)
     }
   }
 }
