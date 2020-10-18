5254a00b2357e02a5bd984bea2af08f199a55996
Darren Huang <dh475.cs.washington.edu>
2020-10-18 06:15:02 -0700

    working on create Community

--- pages/testingPage.vue
+++ pages/testingPage.vue
@@ -1,42 +1,41 @@
 <template>
 <section class="container">
 <div>
-      <h2>
-        Write to Firestore.
-      </h2>
-      <div>
-        <button @click="getCommunityInfo" :disabled="writeSuccessful">
-          <span v-if="!writeSuccessful">get community</span>
-          <span v-else>Successful!
-          </span>
-        </button>
-      </div>
-
-      <div>
-        <button @click="createCommunityInfo" :disabled="writeSuccessful">
-          <span v-if="!createCommunityInfo">create community</span>
-          <span v-else>Successful!
-          </span>
-        </button>
-      </div>
-    </div>
+  <h2>
+    Write to Firestore.
+  </h2>
+  <div>
+    <button @click="getCommunityInfo" :disabled="writeSuccessful">
+      <span v-if="!writeSuccessful">get community</span>
+      <span v-else>Successful!
+      </span>
+    </button>
+  </div>
+  <div>
+    <button @click="createCommunity" :disabled="writeSuccessful">
+      <span v-if="!createCommunity">create community</span>
+      <span v-else>Successful!
+      </span>
+    </button>
+  </div>
+</div>
 </section>
 </template>
 <script>
 const community = {
-        name: '10',
-        numUsers: '4',
-        numGroups: 1,
-        description: 'Hackathons',
-        groupIDs: [{
-          groupOne: 'hack1',
-          groupTwo: 'hack2',
-          groupThree: 'hack3'
-        }],
-        cumExp: 7,
-        popRank: 5,
-        imgurl: 'http://www.clker.com/cliparts/e/d/8/2/1314062692434832210campfire.png'
-      }
+  name: '10',
+  numUsers: '4',
+  numGroups: 1,
+  description: 'Hackathons',
+  groupIDs: [{
+    groupOne: 'hack1',
+    groupTwo: 'hack2',
+    groupThree: 'hack3'
+  }],
+  cumExp: 7,
+  popRank: 5,
+  imgurl: 'http://www.clker.com/cliparts/e/d/8/2/1314062692434832210campfire.png'
+}
 export default {
 
   data () {
@@ -54,10 +53,11 @@ export default {
         console.log(e)
       }
     },
-    async createCommunityInfo () {
-       try {
-        await this.$createCommunityInfo(community).then((res) => {
-          console.log(res.key)
+    async createCommunity () {
+      try {
+        console.log('trying to create a community:')
+        await this.$createCommunity(community).then((res) => {
+          console.log(res)
         })
       } catch (e) {
         console.log(e)
