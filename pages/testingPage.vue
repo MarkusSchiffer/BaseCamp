495b610649d67fe228538bf2319bb7b0bbcc57a6
Darren Huang <dh475.cs.washington.edu>
2020-10-18 05:42:54 -0700

    include assest for db

--- pages/testingPage.vue
+++ pages/testingPage.vue
@@ -6,7 +6,15 @@
       </h2>
       <div>
         <button @click="getCommunityInfo" :disabled="writeSuccessful">
-          <span v-if="!writeSuccessful">Write now</span>
+          <span v-if="!writeSuccessful">get community</span>
+          <span v-else>Successful!
+          </span>
+        </button>
+      </div>
+
+      <div>
+        <button @click="createCommunityInfo" :disabled="writeSuccessful">
+          <span v-if="!createCommunityInfo">create community</span>
           <span v-else>Successful!
           </span>
         </button>
@@ -15,8 +23,22 @@
 </section>
 </template>
 <script>
-
+const community = {
+        name: '10',
+        numUsers: '4',
+        numGroups: 1,
+        description: 'Hackathons',
+        groupIDs: [{
+          groupOne: 'hack1',
+          groupTwo: 'hack2',
+          groupThree: 'hack3'
+        }],
+        cumExp: 7,
+        popRank: 5,
+        imgurl: 'http://www.clker.com/cliparts/e/d/8/2/1314062692434832210campfire.png'
+      }
 export default {
+
   data () {
     return {
       writeSuccessful: false
@@ -27,6 +49,15 @@ export default {
       try {
         await this.$getCommunityInfo('-MJv5n-b4hVQgcvOVaAn').then((res) => {
           console.log('inside testing page, result = ' + res)
+        })
+      } catch (e) {
+        console.log(e)
+      }
+    },
+    async createCommunityInfo () {
+       try {
+        await this.$createCommunityInfo(community).then((res) => {
+          console.log(res.key)
         })
       } catch (e) {
         console.log(e)
